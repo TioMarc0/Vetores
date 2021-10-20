e6a83dde7b5b26c6adc1823a372003f3a764063a
Tio Marco (TioDarius1) <91704772+TioMarc0@users.noreply.github.com>
2021-10-20 12:41:21 -0300

    Update

--- src/VetorMatriz1/leia5Num.java
+++ src/VetorMatriz1/leia5Num.java
@@ -1,3 +1,4 @@
+package VetorMatriz1;
 import java.util.Scanner;
 
 public class leia5Num {
---
+++ src/VetorMatriz2/valorImpar.java
@@ -0,0 +1,23 @@
+package VetorMatriz2;
+
+public class valorImpar {
+//Faça um programa que peça ao usuário um valor inteiro impar entre 3 e 11 (inclusive 3 e
+//11, valide esta entrada), após isto crie uma matriz quadrada (inteiros) do tamanho do número
+//	informado pelo usuário. Permita que o usuário informe os valores para cada uma das
+//	posições da matriz. Após isto calcula e mostre as seguintes informações sobre a matriz:
+//	 1-	Soma dos elementos
+//	2- 	Média dos elementos (duas casas decimais)
+//	3- 	Maior valor
+//	4- 	Menor valor
+//	5- 	Contador de pares
+//	6- 	Contador de ímpares
+//	7- 	Soma da diagonal principal
+//	8- 	Soma da diagonal secundária
+//	9-	Mostrar a Matriz (formatada)
+//	10-	Permitir gravar e ler estes dados de um arquivo
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
