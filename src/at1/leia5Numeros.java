8d0fab083742ebf7e71f3eeaed88d46095ea265e
Tio Marco (TioDarius1) <91704772+TioMarc0@users.noreply.github.com>
2021-10-20 11:56:54 -0300

    commit

--- src/at1/leia5Numeros.java
+++ src/at1/leia5Numeros.java
@@ -5,16 +5,17 @@ import java.util.Scanner;
 public class leia5Numeros {
 
 //Faça um Programa que leia um vetor de 5 números inteiros e mostre-os.
+	 String msg 
 	Scanner ent = new Scanner(System.in);
+	 int x;
+	
+	
 		
-		private int x;
-		private String msg ;
-		
-			public int[] num1 = new int[x];
-			public String msg () {
-				for (int x = 0; x < 5; x++);
-					num1[x] = ent.nextInt();
-					return msg += "Posição do vetor: [" + x + "] | Valor armazenado: " + num1[x] + "\n";
+		public int[] num1 = new int[x];
+		public String msg () {
+			for (int x = 0; x < 5; x++);
+			num1[x] = ent.nextInt();
+			msg += "Posição do vetor: [" + x + "] | Valor armazenado: " + num1[x] + "\n";
 		}
 	
 	{
