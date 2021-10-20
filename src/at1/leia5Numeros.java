094fb107a96ca81c6c6693841760554725e66148
Tio Marco (TioDarius1) <91704772+TioMarc0@users.noreply.github.com>
2021-10-20 11:11:19 -0300

    commiting

--- src/at1/leia5Numeros.java
+++ src/at1/leia5Numeros.java
@@ -1,31 +1,32 @@
 package at1;
 
 import java.util.Scanner;
-public class leia5Numeros{
+
+public class leia5Numeros {
 
 //Faça um Programa que leia um vetor de 5 números inteiros e mostre-os.
-public int[] num1 = new int[5];
- public String msg = "";
- {
-		
-	
 	Scanner ent = new Scanner(System.in);
+		
+		private int x;
+		private String msg ;
+		
+			public int[] num1 = new int[x];
+			public String msg () {
+				for (int x = 0; x < 5; x++);
+					num1[x] = ent.nextInt();
+					return msg += "Posição do vetor: [" + x + "] | Valor armazenado: " + num1[x] + "\n";
+		}
 	
-	
-	
-
-	for(
-	int x = 0;x<5;x++)
 	{
 
-		num1[x] = ent.nextInt();
-
-		msg += "Posição do vetor: [" + x + "] | Valor armazenado: " + num1[x] + "\n";
-
-	}
-
 	
-	ent.close();
-}
+
+		
 
 }
+	
+}
+
+		
+
+
--- src/at1/leia5NumerosMain.java
+++ src/at1/leia5NumerosMain.java
@@ -1,17 +1,14 @@
 package at1;
 
-public class leia5NumerosMain{
-
+public class leia5NumerosMain {
 
 //Faça um Programa que leia um vetor de 5 números inteiros e mostre-os.
 
-public static void main(String[] args) {
-	
-	leia5Numeros ent = new leia5Numeros();
-	System.out.println(msg);
-	
-}
+	public static void main(String[] args) {
 
-
-
+		leia5Numeros ent = new leia5Numeros();
+		System.out.println(msg);
+		ent.close();
 	}
+
+}
