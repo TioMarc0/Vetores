c436c2a2c6bbe32abf7887a4665b83fb046aaa0f
Tio Marco (TioDarius1) <91704772+TioMarc0@users.noreply.github.com>
2021-10-20 12:36:45 -0300

    Update leia5Num.java

--- src/leia5Num.java
+++ src/leia5Num.java
@@ -6,17 +6,17 @@ public class leia5Num {
 		// TODO Auto-generated method stub
 		//Faça um Programa que leia um vetor de 5 números inteiros e mostre-os.
 		
-		int[] num1 = new int[1];
-		String msg = "";
+		int[] num1 = new int[5];
+		String msg = "" ;
 		Scanner ent = new Scanner(System.in);
 		
 		
 		
-		for(int x = 0; x < 5; x++){
+		for(int x = 0; x < 1; x++){
 
-		    num1[x] = ent.nextInt();
+		    num1[x] = ent.nextInt();{
 
-		    msg += "Posição do vetor: [" + x + "] | Valor armazenado: " + num1[x] + "\n";
+		    msg += "Posição do vetor: [" + x + "] | Valor armazenado: " + num1[x] + "\n";}
 
 		}
 
